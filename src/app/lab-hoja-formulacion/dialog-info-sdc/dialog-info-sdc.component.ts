a5db7e8592f1c17102027455a406b612343263d3
precotex2025 <202682732+hmedina1402-Prx@users.noreply.github.com>
2026-07-10 19:09:09 -0500

    feat: cambio colorante optico
    :cambio colorante optico

--- src/app/lab-hoja-formulacion/dialog-info-sdc/dialog-info-sdc.component.ts
+++ src/app/lab-hoja-formulacion/dialog-info-sdc/dialog-info-sdc.component.ts
@@ -50,8 +50,7 @@ interface informacionSDCProduccion{
   cod_OrdTra_Ref: string;
   tela_Ref: string;
   rel_Ban_Ref: string;
-  lote_Ref: string;
-  des_Colorante_Optico: string; //Nuevo
+  lote_Ref: string
 }
 
 @Component({
